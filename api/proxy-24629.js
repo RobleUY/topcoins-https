55c5c830e4983b4f9b82562370fff636b31b8443
RobleUY <jusemadi13@gmail.com>
2026-06-06 07:32:39 -0300

    Add files via upload

---
+++ api/proxy-24629.js
@@ -0,0 +1,50 @@
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(request) {
+  const url = new URL(request.url);
+  
+  // Extraer el path después de /proxy/24629/
+  const pathMatch = url.pathname.match(/\/proxy\/24629\/(.*)/);
+  const targetPath = pathMatch ? pathMatch[1] : '';
+  
+  // Construir la URL de destino
+  const targetUrl = `http://june.hidencloud.com:24629/${targetPath}${url.search}`;
+  
+  try {
+    // Hacer la petición al servidor HTTP original
+    const response = await fetch(targetUrl, {
+      method: request.method,
+      headers: {
+        ...Object.fromEntries(request.headers),
+        host: 'june.hidencloud.com',
+      },
+      body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined,
+    });
+
+    // Crear nueva respuesta con los headers apropiados
+    const responseHeaders = new Headers(response.headers);
+    responseHeaders.set('Access-Control-Allow-Origin', '*');
+    responseHeaders.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    responseHeaders.set('Access-Control-Allow-Headers', '*');
+
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: responseHeaders,
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ 
+        error: 'Error en el proxy',
+        message: error.message,
+        targetUrl 
+      }), 
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+}
